ca9f232340751b147d23da59ebbaf0235297fb78
Yakup <yakupjoraev@gmail.com>
2023-06-01 21:04:34 +0300

    added new section and fixed

--- src/js/main.js
+++ src/js/main.js
@@ -61,24 +61,77 @@ function heroBg() {
 
   link2.addEventListener('mouseenter', () => {
     container.classList.add('transparent')
+    document.querySelectorAll('.menu__item-link').forEach(element => {
+      element.style.color = '#fff'
+    });
   })
 
   link2.addEventListener('mouseout', () => {
     container.classList.remove('transparent')
+
+    document.querySelectorAll('.menu__item-link').forEach(element => {
+      element.style.color = '#343a40'
+    });
   })
 
   link3.addEventListener('mouseenter', () => {
     container.classList.add('transparent')
+
+    document.querySelectorAll('.menu__item-link').forEach(element => {
+      element.style.color = '#fff'
+    });
   })
 
   link3.addEventListener('mouseout', () => {
     container.classList.remove('transparent')
+
+    document.querySelectorAll('.menu__item-link').forEach(element => {
+      element.style.color = '#343a40'
+    });
   })
 
 
 }
 
 heroBg();
+
+
+
+// Аккордеон
+const accordionItems = document.querySelectorAll('[data-accordion-item]');
+let openAccordion = null; // переменная для хранения ссылки на открытый аккордеон
+
+function toggleAccordion() {
+  if (openAccordion && openAccordion !== this) {
+    // Если есть открытый аккордеон, который не совпадает с текущим
+    openAccordion.classList.remove('active'); // закрыть его
+    const openAccordionContent = openAccordion.nextElementSibling;
+    if (openAccordionContent) {
+      // если у аккордеона есть содержимое
+      openAccordionContent.style.maxHeight = null; // сбросить высоту контента
+    }
+  }
+
+  this.classList.toggle('active'); // открыть или закрыть текущий аккордеон
+
+  const content = this.nextElementSibling;
+  if (content) {
+    // если у аккордеона есть содержимое
+    if (content.style.maxHeight) {
+      // Если контент открыт, закрыть его
+      content.style.maxHeight = null;
+    } else {
+      // Если контент закрыт, открыть его
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+
+  openAccordion = this; // запомнить ссылку на открытый аккордеон
+}
+
+
+accordionItems.forEach(item => item.addEventListener('click', toggleAccordion));
+
 
 AOS.init({
   disable: 'phone',
